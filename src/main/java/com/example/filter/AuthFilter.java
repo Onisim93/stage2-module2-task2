d4350edd340139e0d0f18f4092c26ac0e8621bc0
onisim_albarov <onisim93@gmail.com>
2022-12-25 15:36:38 +0400

    fixed AuthFilter

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -2,15 +2,19 @@ package com.example.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/users/*")
+@WebFilter(urlPatterns = "/user/*")
 public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String user = servletRequest.getParameter("user");
-        if (user == null || user.isEmpty()) {
-            servletRequest.getRequestDispatcher("/login.jsp").forward(servletRequest, servletResponse);
+        HttpSession session = ((HttpServletRequest) servletRequest).getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            ((HttpServletResponse) servletResponse).sendRedirect("/login.jsp");
         }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
