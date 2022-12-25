13ead9218536afbf618346061da48f896ebbd2a5
onisim_albarov <onisim93@gmail.com>
2022-12-25 15:11:25 +0400

    completed stage2-module2-task2

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,16 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/users/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String user = servletRequest.getParameter("user");
+        if (user == null || user.isEmpty()) {
+            servletRequest.getRequestDispatcher("/login.jsp").forward(servletRequest, servletResponse);
+        }
+    }
 }
